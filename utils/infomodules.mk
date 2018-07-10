3d8533fbc657cf9c7706fee6577a74f2baede883
Zhao Owen <kikakkz@hotmail.com>
2018-07-11 00:04:54 +0800

    Also echo repository name for easy search

--- utils/infomodules.mk
+++ utils/infomodules.mk
@@ -2,5 +2,6 @@ infomodules = $(submodules:%=info-%)
 
 $(infomodules):
 	@if test -d $(sourcedir)/$(@:info-%=%); then						\
+		echo $(sourcedir)/$(@:info-%=%);								\
 		cd $(sourcedir)/$(@:info-%=%); git status;						\
 	fi
